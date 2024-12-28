9866fe0b41c1508179a9507237fa059190e0999d
SeawaysiS <seawaydev@gmail.com>
2024-12-28 15:51:11 +0700

    update api graphql

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -1,31 +1,61 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
 type ResponseData = {
   message: string;
 };
-
+interface pokemon {
+  id:string,
+    number:string,
+    attacks : {
+      fast: {
+        name:string,
+        type:string,
+        damage:number
+      }
+    },
+    name : string,
+    evolutions : {
+      id : string
+    }
+}
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   if (req.method === "POST") {
     const {search_text} = req.body;
-
     //console.log(process.env.endpointGrapql);
-    console.log(search_text);
-    // try{
-    // const response = await fetch("https://graphql-pokemon2.vercel.app", {
-    //   method: "POST",
-    //   body: formData,
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
-    //     console.log(response);
-    //   } catch (e) {
-    //     console.log((e as Error).message);
-    //   }
-    res.status(200).json({ message: `pokemon ${search_text}` });
+
+    const query: string = JSON.stringify({
+      query: `query pokemon($name: String){
+            pokemon(name : $name){
+            id,
+            number,
+            attacks{
+              fast{
+                name,
+                type,
+                damage
+              }
+            },
+            name,
+            evolutions{
+              id
+            }
+          }
+        }
+    `,
+      variables: { name: search_text },
+    });
+    const data: pokemon = await fetch(process.env.endpointGrapql as string, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: query,
+    }).then((res) => res.json());
+
+    console.log(data);
+    res.status(200).json({ message: 'test' });
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
   }
